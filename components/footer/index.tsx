29f7195397b84d4864f79d8e0d32e95949db9626
Allen OuYang <ouyangzhou@xylink.com>
2019-12-08 03:30:15 +0800

    style: add website approve

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -3,7 +3,7 @@ import style from './style/index.scss';
 const FooterComponent = () => {
   return (
     <div className={style.footer}>
-      <h4>By Allen OYang</h4>
+      <a href="http://www.beian.miit.gov.cn/">京ICP备19055693号</a>
     </div>
   )
 }
--- config/api.config.ts
+++ config/api.config.ts
@@ -1,6 +1,7 @@
 const apiMap = {
   development: {
     baseURL: 'http://localhost:7001',
+    // baseURL: 'https://www.utauu.com',
   },
   production: {
     baseURL: 'https://www.utauu.com',
